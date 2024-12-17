00f8114396ebe1f09e66ce9ac49817587105fdcc
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2024-12-17 10:15:24 +0500

    change link

--- src/components/ProrektorNavbar/index.jsx
+++ src/components/ProrektorNavbar/index.jsx
@@ -44,8 +44,8 @@ const ProrektorNavbar = () => {
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            to="talabnoma"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
@@ -55,7 +55,7 @@ const ProrektorNavbar = () => {
             <span className="self-center text-lg sm:text-2xl font-semibold whitespace-nowrap dark:text-white">
               ombor
             </span>
-          </a>
+          </Link>
 
           {/* User Menu Button */}
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative">
